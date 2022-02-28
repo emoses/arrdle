3c68e9930cd4d9a7b1df061c0646d8063491714d
Evan Moses <evan.moses@okta.com>
2022-02-28 13:49:11 -0800

    Try copy-to-clipboard

--- src/arrdle/events.cljs
+++ src/arrdle/events.cljs
@@ -76,12 +76,28 @@
  (fn [_ [_ new-modal]]
    new-modal))
 
+(defn copy-to-clipboard [val]
+  (let [el (js/document.createElement "textarea")]
+    (set! (.-value el) val)
+    (set! (.-contentEditable el) true)
+    (.appendChild js/document.body el)
+    (.select el)
+    (js/document.execCommand "copy")
+    (.removeChild js/document.body el)))
+
+
 (rf/reg-event-db
  ::share-social-clipboard
  (fn [db [_ text]]
-   (-> (.writeText (.-clipboard js/navigator) text)
-       (.then #(rf/dispatch [::share-social-clipboard-done]))
-       (.catch #(rf/dispatch [::share-social-clipboard-error])))
+   (if (.-clipboard js/navigator)
+     (-> (.writeText (.-clipboard js/navigator) text)
+         (.then #(rf/dispatch [::share-social-clipboard-done]))
+         (.catch #(rf/dispatch [::share-social-clipboard-error])))
+     (try
+       (copy-to-clipboard text)
+       (rf/dispatch [::share-social-clipboard-done])
+       (catch js/Error e
+         (rf/dispatch [::share-social-clipboard-error e]))))
    db))
 
 (rf/reg-event-db
